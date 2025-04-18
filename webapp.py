98012d474ff65c0a8e59471b5d92fcbae8a103b1
byte4geek <admin@byte4geek.com>
2025-04-18 14:48:05 +0200

    First release

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 import logging
 
-# Configure base logging
+# Configure base logging 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load variables
